02510e2d22f7313d43fd810d540127375f7757e0
DeAristyo <dimas.aristyo@gmail.com>
2022-02-01 00:16:25 +0700

    next.js_practice

---
+++ pages/cars/[id].js
@@ -0,0 +1,50 @@
+import { useRouter } from "next/router";
+import Head from "next/head";
+
+const Car = ({ car }) => {
+    const router = useRouter();
+    const { id } = router.query;
+
+    return (
+        <div>
+            <Head>
+                <title>Halaman Car</title>
+            </Head>
+            <h1>Hello {id}</h1>
+            <img src={car.image} width='30rem' />
+        </div>
+    );
+};
+export default Car;
+
+export async function getServerSideProps({ params }) {
+    const req = await fetch(`http://localhost:3000/${params.id}.json`);
+    const data = await req.json();
+
+    return {
+        props: { car: data },
+    };
+}
+
+// export async function getStaticProps({ params }) {
+//     const req = await fetch(`http://localhost:3000/${params.id}.json`);
+//     const data = await req.json();
+
+//     return {
+//         props: { car: data },
+//     };
+// }
+
+// export async function getStaticPaths() {
+//     const req = await fetch('http://localhost:3000/cars.json');
+//     const data = await req.json();
+
+//     const paths = data.map((car) => {
+//         return { params: { id: car } };
+//     });
+
+//     return {
+//         paths,
+//         fallback: false,
+//     };
+// }
---
+++ pages/hello.js
@@ -0,0 +1,6 @@
+const Home = () => {
+    return (
+        <h1>Hi Mom</h1>
+    );
+};
+export default Home;
